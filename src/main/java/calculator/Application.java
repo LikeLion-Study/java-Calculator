1d84ef8f11aa45f9cf1d3bb2a5666c7ec0328716
김의현 <hyunkim6790@gmail.com>
2025-03-24 11:40:01 +0900

    init commiit

---
+++ src/main/java/calculator/Application.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Application {
+    public static void main(String[] args) {
+        // TODO: 코드 작성
+
+        // 예시 코드
+        Calculator calculator = new Calculator();
+        calculator.calculate("");
+    }
+}
---
+++ src/test/java/calculator/ApplicationTest.java
@@ -0,0 +1,37 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void 빈_문자열_입력_시_0을_반환한다() {
+        // 아직 calculate 함수가 구현되지 않은 상태에서 TDD Red Test용 코드
+        int result = calculator.calculate("");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void 덧셈과_뺄셈만_포함된_수식_계산() {
+        int result = calculator.calculate("1+2-3");
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    public void 곱셈과_덧셈_포함_수식_계산() {
+        int result = calculator.calculate("2*3+4");
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    public void 잘못된_수식_입력_시_예외_발생() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            calculator.calculate("1++2");
+        });
+    }
+
+    // TODO: 테스트 코드 추가 가능합니다.
+}
